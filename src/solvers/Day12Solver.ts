4a3c073ecdd195b166babdb05fb57cfdb7945e7a
barkliam <barkleyliam@gmail.com>
2024-12-13 21:17:58 +0800

    Day 12 final refactoring

--- src/solvers/Day12Solver.ts
+++ src/solvers/Day12Solver.ts
@@ -2,13 +2,15 @@ import {PuzzleSolver} from "./PuzzleSolver";
 import {Point} from "../utility/Point";
 
 type GardenPlot = {
-    interiorPoints: Array<Point>;
-    borderingPoints: Array<BorderPoint>;
+    interiorPoints: Array<Point>; borderingPoints: Array<BorderPoint>;
 };
 
 type BorderPoint = {
-    point: Point;
-    closestAreaPoint: Point;
+    point: Point; adjacentInteriorPoint: Point;
+};
+
+type AreaSide = {
+    points: Array<BorderPoint>;
 };
 
 export default class Day12Solver extends PuzzleSolver {
@@ -27,43 +29,29 @@ export default class Day12Solver extends PuzzleSolver {
         let price = 0;
 
         for (const gardenPlot of this.gardenPlots) {
-            price +=
-                this.calculateNumberOfSides(gardenPlot) *
-                gardenPlot.interiorPoints.length;
+            price += this.calculateNumberOfSides(gardenPlot) * gardenPlot.interiorPoints.length;
         }
         return price;
     }
 
     calculateNumberOfSides(gardenPlot: GardenPlot): number {
-        return this.expandSides([...gardenPlot.borderingPoints]).length;
+        return this.findStraightSides([...gardenPlot.borderingPoints]).length;
     }
 
-    expandSides(borderingPoints: Array<BorderPoint>): Array<Array<BorderPoint>> {
-        const sides: Array<Array<BorderPoint>> = [];
+    findStraightSides(borderingPoints: Array<BorderPoint>): Array<AreaSide> {
+        const sides: Array<AreaSide> = [];
         while (borderingPoints.length > 0) {
             const newSide: Array<BorderPoint> = [];
-            const firstPoint = borderingPoints.pop()!;
-            newSide.push(firstPoint);
-            const queue: Array<BorderPoint> = [firstPoint];
-            while (queue.length > 0) {
-                const current = queue.shift();
-                if (!current) continue;
-                const possibleNextBorders = current.point.directlyAdjacent();
-                for (const borderPoint of borderingPoints) {
-                    if (
-                        !newSide.includes(borderPoint) &&
-                        possibleNextBorders.includes(borderPoint.point) &&
-                        current.closestAreaPoint.isDirectlyAdjacent(
-                            borderPoint.closestAreaPoint
-                        )
-                    ) {
-                        newSide.push(borderPoint);
-                        queue.push(borderPoint);
-                    }
-                }
+            const areaPointsToAdd: Array<BorderPoint> = [borderingPoints.pop()!];
+
+            while (areaPointsToAdd.length > 0) {
+                const current = areaPointsToAdd.pop()!;
+                newSide.push(current);
+                const nextToAddSidePoints = borderingPoints.filter((borderPoint) => current.point.isDirectlyAdjacent(borderPoint.point) && current.adjacentInteriorPoint.isDirectlyAdjacent(borderPoint.adjacentInteriorPoint));
+                areaPointsToAdd.push(...nextToAddSidePoints);
+                borderingPoints = borderingPoints.filter((borderPoint) => !nextToAddSidePoints.includes(borderPoint));
             }
-            sides.push(newSide);
-            borderingPoints = borderingPoints.filter((bp) => !newSide.includes(bp));
+            sides.push({points: newSide});
         }
         return sides;
     }
@@ -91,25 +79,24 @@ export default class Day12Solver extends PuzzleSolver {
             const interiorPoints: Array<Point> = [];
             let borderingPoints: Array<BorderPoint> = [];
             const areaPointsToAdd: Array<Point> = [allPoints.pop()!];
+
             while (areaPointsToAdd.length > 0) {
                 const current = areaPointsToAdd.pop()!;
                 interiorPoints.push(current);
+
                 current.directlyAdjacent().forEach((adjacentPoint) => {
                     const index = allPoints.indexOf(adjacentPoint);
                     if (index !== -1) {
                         areaPointsToAdd.push(adjacentPoint);
                         allPoints.splice(index, 1);
-                    } else if (
-                        !interiorPoints.includes(adjacentPoint) &&
-                        !areaPointsToAdd.includes(adjacentPoint)
-                    ) {
+                    } else if (!interiorPoints.includes(adjacentPoint) && !areaPointsToAdd.includes(adjacentPoint)) {
                         borderingPoints.push({
-                            point: adjacentPoint,
-                            closestAreaPoint: current,
+                            point: adjacentPoint, adjacentInteriorPoint: current,
                         });
                     }
                 });
             }
+
             gardenPlots.push({interiorPoints, borderingPoints});
         }
         return gardenPlots;
